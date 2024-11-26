f79d0f9081538bc693d1d12497c50650c1c59310
kaizen-nagata <r.nagata@kaizen-style.com>
2024-11-26 12:15:48 +0900

    Initial Commit

---
+++ X_SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  X_SampleApp
+//
+//  Created by r.nagata on 2024/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ X_SampleAppTests/X_SampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  X_SampleAppTests.swift
+//  X_SampleAppTests
+//
+//  Created by r.nagata on 2024/11/26.
+//
+
+import Testing
+@testable import X_SampleApp
+
+struct X_SampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
